6302332e3c3d6b60c83a504a401562973274c0fd
chunjie <wcj@ebj.sx.cn>
2025-03-12 21:55:17 +0800

    菜单效果调整

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -1,3 +1,15 @@
 function toSubmit() {
     $("#password").val(enc($("#password").val(), $(".login-title").attr('data-key'), $(".login-title").attr('data-v')));
 }
+
+const path = window.location.pathname;
+$(".sider .nav-item").each(function () {
+    //直接设置焦点
+    $(this).removeClass("active")
+    if ($(this).children(".nav-link").attr("href") == path) {
+        $(this).addClass("active");
+        if ($(this).parent().children(".sub-item").length > 0) {
+            $(this).parent().parent().addClass("open");
+        }
+    }
+});
