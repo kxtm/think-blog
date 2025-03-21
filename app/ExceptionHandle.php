1d75b524c3b3e5000bfea2101dd620399baf3982
chunjie <wcj@ebj.sx.cn>
2025-03-22 01:54:27 +0800

    增加错误页面配置

--- app/ExceptionHandle.php
+++ app/ExceptionHandle.php
@@ -51,7 +51,9 @@ class ExceptionHandle extends Handle
     public function render($request, Throwable $e): Response
     {
         // 添加自定义异常处理机制
-
+        if ($e instanceof HttpException && $request->isAjax()) {
+            return response($e->getMessage(), $e->getStatusCode());
+        }
         // 其他错误交给系统处理
         return parent::render($request, $e);
     }
